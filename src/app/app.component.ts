8376112b04f1ce65cd9633a0785aca19332ae0f9
Fahad <fahad.shoukat111@gmail.com>
2024-09-24 11:31:46 +0500

    navbar, banner ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { BannerComponent } from "./components/banner/banner.component";
+import { BrandComponent } from "./components/brand/brand.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, BannerComponent, BrandComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
